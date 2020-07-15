2b3abe7c7fdf362065bf10ae276baf6fdc6a923d
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2020-07-15 12:13:21 +0530

    Additional Styling

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import avatar from "../img/profile-boy.png";
 
 const Dashboard = () => {
   return (
     <div className="container">
       <div className="row">
         <div className="col-1-3">
-          <div className="card card-base">A</div>
+          <div className="card card-base">
+            <div className="card-body">
+              <div className="card-header" />
+              <img src={avatar} alt="Avatar" className="card-img" />
+            </div>
+          </div>
         </div>
         <div className="col">
           <div className="card card-base">A</div>
